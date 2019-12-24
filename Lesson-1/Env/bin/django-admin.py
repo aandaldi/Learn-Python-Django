79ad65d2fd6e2d43a8edb10a7357fc37a6976562
aandaldi <aandaldi@gmail.com>
2019-12-24 11:21:34 +0700

    create simple app

---
+++ Lesson-1/Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aandaldi/Documents/TrainInternal/Learn-Python-Django/Lesson-1/Env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
